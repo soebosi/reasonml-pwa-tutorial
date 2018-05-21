52edf4cb44adf2476f557b81e7611242662628c7
ebosi38051 <ebosi38051@gmail.com>
2018-05-22 00:48:03 +0900

    Move public resource -> public directory

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const outputDir = path.join(__dirname, "build/");
+const outputDir = path.join(__dirname, "public/");
 
 const isProd = process.env.NODE_ENV === 'production';
 
