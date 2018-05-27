563279903b4de2eaf9851012621387dcd9ee4622
ebosi38051 <ebosi38051@gmail.com>
2018-05-27 22:22:25 +0900

    Use rocket for server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,37 +1,13 @@
-use std::fs::File;
-use std::io::prelude::*;
-use std::net::TcpListener;
-use std::net::TcpStream;
+#![feature(plugin)]
+#![plugin(rocket_codegen)]
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+extern crate rocket;
 
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-
-        let _result = handle_connection(stream);
-    }
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
 }
 
-fn handle_connection(mut stream: TcpStream) -> std::io::Result<()> {
-    let mut buffer = [0; 512];
-    stream.read(&mut buffer).unwrap();
-
-    let get = b"GET / HTTP/1.1\r\n";
-
-    let (status_line, filename) = if buffer.starts_with(get) {
-        ("HTTP/1.1 200 OK\r\n\r\n", "index.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
-    };
-
-    let mut contents = String::new();
-    let mut file = File::open("../public/".to_string() + filename)?;
-    file.read_to_string(&mut contents)?;
-
-    let response = format!("{}{}", status_line, contents);
-
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
-    Ok(())
+fn main() {
+    rocket::ignite().mount("/", routes![index]).launch();
 }
