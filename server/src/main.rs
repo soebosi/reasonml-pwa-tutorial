4b53fd78fccb5278120f3cc8d9888d284f1a55d3
ebosi38051 <ebosi38051@gmail.com>
2018-05-28 00:42:29 +0900

    Implement static file server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,13 +1,35 @@
-#![feature(plugin)]
+#![feature(plugin, decl_macro)]
 #![plugin(rocket_codegen)]
 
 extern crate rocket;
 
+#[cfg(test)]
+mod tests;
+
+use std::io;
+use std::path::{Path, PathBuf};
+
+use rocket::response::NamedFile;
+
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index() -> io::Result<NamedFile> {
+    NamedFile::open("../public/index.html")
+}
+
+#[get("/public/<file..>", rank = 2)]
+fn files(file: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("../public/").join(file)).ok()
+}
+
+#[get("/<_file..>", rank = 3)]
+fn fallback(_file: PathBuf) -> io::Result<NamedFile> {
+    NamedFile::open("../public/index.html")
+}
+
+fn rocket() -> rocket::Rocket {
+    rocket::ignite().mount("/", routes![index, files, fallback])
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket().launch();
 }
