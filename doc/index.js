5617fc0772bed1afca46cd05ab232a39968bbf7c
ebosi38051 <ebosi38051@gmail.com>
2018-05-30 00:48:13 +0900

    Fix dot -> svg generator

--- doc/index.js
+++ doc/index.js
@@ -4,10 +4,17 @@ const fs                 = require('fs').promises;
 
 const viz = new Viz({ Module, render });
 
-async function generateSVG(dotfile) {
-  const dot    = await fs.readFile(dotfile, 'utf8');
-  const result = await viz.renderString(dot);
-  console.log(result);
+async function generateSVG(name) {
+  const dotfile = `${name}.dot`;
+  const svgfile = `${name}.svg`;
+  const dot = await fs.readFile(dotfile, 'utf8');
+  try {
+    const result = await viz.renderString(dot);
+    await fs.writeFile(svgfile, result);
+    console.log('generated', svgfile);
+  } catch(e) {
+    console.error(e);
+  }
 }
 
-generateSVG('client_architecture.dot');
+generateSVG('client_architecture');
