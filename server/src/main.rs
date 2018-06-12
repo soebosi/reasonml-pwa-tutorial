2ed2c45a183534681dcf0ef6448d41b7faa4428d
ebosi38051 <ebosi38051@gmail.com>
2018-06-13 01:11:53 +0900

    Impelemnt item's id with sequence_number

--- server/src/main.rs
+++ server/src/main.rs
@@ -3,21 +3,24 @@
 
 extern crate rocket;
 
-#[macro_use] extern crate rocket_contrib;
-#[macro_use] extern crate serde_derive;
-
-#[cfg(test)]
-mod tests;
+#[macro_use]
+extern crate rocket_contrib;
+#[macro_use]
+extern crate serde_derive;
 
 use std::io;
 use std::path::{Path, PathBuf};
+use std::sync::atomic::{AtomicUsize, Ordering};
 
 use rocket_contrib::{Json, Value};
 use rocket::response::NamedFile;
+use rocket::State;
+
+struct SequenceNumber(AtomicUsize);
 
 #[derive(Serialize, Deserialize)]
 struct Message {
-    name: String
+    name: String,
 }
 
 #[get("/")]
@@ -26,8 +29,12 @@ fn index() -> io::Result<NamedFile> {
 }
 
 #[post("/api/v1/items", format = "application/json", data = "<message>")]
-fn new_item(message: Json<Message>) -> Json<Value> {
-    Json(json!({ "id": "xxxx", "name": message.name }))
+fn new_item(message: Json<Message>, sequence_number: State<SequenceNumber>) -> Json<Value> {
+    sequence_number.0.fetch_add(1, Ordering::Relaxed);
+    Json(json!({
+      "id": sequence_number.0.load(Ordering::Relaxed).to_string(),
+      "name": message.name,
+    }))
 }
 
 #[get("/static/<file..>", rank = 2)]
@@ -41,7 +48,10 @@ fn fallback(_file: PathBuf) -> io::Result<NamedFile> {
 }
 
 fn rocket() -> rocket::Rocket {
-    rocket::ignite().mount("/", routes![index, files, fallback, new_item])
+    rocket::ignite()
+        .mount("/", routes![index, files, fallback, new_item])
+        .manage(SequenceNumber(AtomicUsize::new(0)))
+
 }
 
 fn main() {
