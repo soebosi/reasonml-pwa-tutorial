cf8af55d868e57adb4aabe8b8a76d50f1d308ba3
ebosi38051 <ebosi38051@gmail.com>
2018-06-30 00:33:59 +0900

    Fix network pending bug

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,11 +1,8 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js');
 
-workbox.routing.registerRoute(
-  new RegExp('.*\.js'),
-  workbox.strategies.networkFirst()
-);
+workbox.core.setLogLevel(workbox.core.LOG_LEVELS.debug);
 
 workbox.routing.registerRoute(
-  /\.(?:js|css)$/,
-  workbox.strategies.staleWhileRevalidate(),
+  new RegExp('.*'),
+  workbox.strategies.networkFirst()
 );
